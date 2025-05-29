9f2727df3bf2f1c671ed0abc261d505c3a551daf
Aslanbek <asylbek.asan@jihc.edu.kz>
2025-05-29 11:57:10 +0500

    Update booking.dart

--- petprojectsbookingapp/lib/models/booking.dart
+++ petprojectsbookingapp/lib/models/booking.dart
@@ -12,4 +12,24 @@ class Booking {
     required this.time,
     required this.date,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'location': location,
+      'room': room,
+      'time': time,
+      'date': date.toIso8601String(),
+    };
+  }
+
+  factory Booking.fromMap(Map<String, dynamic> map) {
+    return Booking(
+      id: map['id'],
+      location: map['location'],
+      room: map['room'],
+      time: map['time'],
+      date: DateTime.parse(map['date']),
+    );
+  }
 }
