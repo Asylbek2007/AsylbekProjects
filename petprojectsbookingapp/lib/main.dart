53fa0a7ad8649af101b16eab8950cc0e75de62d4
Aslanbek <asylbek.asan@jihc.edu.kz>
2025-05-30 16:52:21 +0500

    Update main.dart

--- petprojectsbookingapp/lib/main.dart
+++ petprojectsbookingapp/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:petprojectsbookingapp/firebase_api.dart';
 import 'package:petprojectsbookingapp/firebase_options.dart';
 import 'package:petprojectsbookingapp/src/features/auth/presentation/pages/splash.dart';
 
