e41102dac52611f78a84725c0d81f6ca2655f0d2
Aslanbek <asylbek.asan@jihc.edu.kz>
2025-09-27 15:58:37 +0500

    Update home_page.dart

--- petprojectsbookingapp/lib/src/features/home/presentation/pages/home_page.dart
+++ petprojectsbookingapp/lib/src/features/home/presentation/pages/home_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:petprojectsbookingapp/models/roombooking.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [Text("Home Page")]));
+    return const RoomBookingScreen();
   }
 }
